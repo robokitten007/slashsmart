73d2ab8a9e5e058366e37013304c313364ef3660
wliu <wliu0709@gmail.com>
2021-07-16 07:18:04 -0400

    added alert window to friend removal button

--- frontend/components/friends/friend_remove.jsx
+++ frontend/components/friends/friend_remove.jsx
@@ -4,15 +4,25 @@ import {Link} from 'react-router-dom';
 class FriendRemove extends React.Component{
 
     constructor(props){
-        super(props)
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
+
+     handleClick(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (confirm("Are you sure you want to delete this friend? This will delete all bills you shared with them")) {
+            this.props.deleteFriend(this.props.id)
+        }
+    }  
 
     render(){
         const {deleteFriend, id} = this.props
         return (
             <div className='friend-setting'>
             <h1>FRIEND SETTINGS</h1>
-            <button onClick ={()=>deleteFriend(id)}>Remove this friend </button>
+            {/* <button onClick ={()=>deleteFriend(id)}>Remove this friend </button> */}
+            <button onClick ={this.handleClick}>Remove this friend </button>
             </div>
         )
     }
