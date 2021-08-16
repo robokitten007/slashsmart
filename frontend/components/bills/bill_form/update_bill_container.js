80aa2cbbc02d5970e0f8de7ad1eea10c94f565b2
wliu <wliu0709@gmail.com>
2021-08-15 20:04:55 -0400

    finish main function of udpate form modal

--- frontend/components/bills/bill_form/update_bill_container.js
+++ frontend/components/bills/bill_form/update_bill_container.js
@@ -5,26 +5,27 @@ import { withRouter } from 'react-router-dom';
 import { openModal, closeModal } from '../../../actions/modal_actions';
 
 const mSTP = (state, ownProps) => {
-    // debugger
-    // console.log("got here");
-    let currentUser=state.entities.users[state.session.id]
     
-     const pathName = ownProps.location.pathname
-     
-    // let friendId= parseInt(ownProps.match.params.userId)
-    let bills = Object.values(state.entities.bills)
-    let arr = pathName.split('/')
-    let billId = parseInt(arr[arr.length-1])
-    let SingleBill
-    bills.forEach((bill)=>{
-        if(bill.id === billId){
-            SingleBill = bill
-        }
-    })
+    let currentUser=state.entities.users[state.session.id]
+    let friends = Object.values(state.entities.friends)
+    let singleBill = state.ui.singleBill
+  
     // console.log(SingleBill)
+    
     return {
         currentUser: currentUser,
-        SingleBill,
+        friends: friends,
+        friendId: singleBill.friend_id,
+        billId: singleBill.id,
+        bill:{
+            user_id: currentUser.id,
+            friend_id: singleBill.friend_id,
+            amount: singleBill.amount,
+            description: singleBill.description,
+            category: singleBill.category,
+            notes: singleBill.notes,
+            paid_by_user: singleBill.paid_by_user
+        },
         formType: 'Update Bill'  
     }
 
@@ -33,7 +34,7 @@ const mSTP = (state, ownProps) => {
 const mDTP = dispatch => ({
     fetchBill: (id)=>dispatch(fetchBill(id)),
     fetchBills: ()=>dispatch(fetchBills()),
-    processForm: (bill) => dispatch(updateBill(bill)),
+    processForm: (bill,id) => dispatch(updateBill(bill, id)),
     closeModal: () => dispatch(closeModal())
 })
 
